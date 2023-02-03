8d066381ca078dd67971bd24b4cc47e7768c2cb8
aurel-tackoen <aurel@opengraphy.com>
2023-02-03 17:54:32 +0100

    feat: start dev

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup></script>
-
+<script setup>
+  const cardAmount = 15;
+</script>
 <template>
-  <main>aaa</main>
+  <main>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div class="cards">
+      <div
+        v-for="index in cardAmount"
+        :key="index"
+        class="card down"
+        :style="`transform: rotateX(60deg) rotateY(0deg) rotateZ(5deg) translateZ(${
+          index * 3
+        }px)`"
+      ></div>
+    </div>
+  </main>
 </template>
